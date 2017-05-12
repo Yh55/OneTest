ee61bca89d6f63d73f38dd781895d5756c375a62
yhh <yhh@xianjinxia.com>
2017-05-12 17:59:13 +0800

    123

---
+++ com/cn/one/info.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+/**
+ * @author Administrator
+ *
+ */
+package com.cn.one;
+
+public class info{
+	
+	
+	public static void main(String[] args) {
+		System.out.println("123");
+	}
+	
+}
